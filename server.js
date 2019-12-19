03aeff5564e5b50db4766e7dcd1443e3139d93e1
juan Ignacio Borda <juanignacioborda@gmail.com>
2019-12-19 17:15:53 -0300

    WIP1

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const port = 3000
-const app = express()
+var app = express()
+app.use(cookieParser())
 const prefix='/static'
+
+
+// set a cookie
+app.get('setcookie/',function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } 
+  else
+  {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
+
+
+app.get('/cookie',function(req, res){
+    res.send(req.cookies)
+})
+
 
 app.use(prefix, express.static('Pictures'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
